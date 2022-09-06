c93dee1b9722f821eb13ff097bde2ef802021331
Indy <exolationyt@gmail.com>
2022-09-06 12:22:12 +0200

    First commit

---
+++ js/utils.js
@@ -0,0 +1,41 @@
+function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (
+        rectangle1.attackBox.postion.x + rectangle1.attackBox.width >= 
+        rectangle2.position.x && 
+        rectangle1.attackBox.postion.x <= 
+        rectangle2.position.x + rectangle2.width &&
+        rectangle1.attackBox.postion.y + rectangle1.attackBox.height >= 
+        rectangle2.position.y && 
+        rectangle1.attackBox.postion.y <= rectangle2.position.y + rectangle2.height
+    )
+}
+
+
+function determineWinner({player, enemy, timerId}) {
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+    if (player.health === enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Tie'
+            } else if (player.health > enemy.health) {
+                document.querySelector('#displayText').innerHTML = 'Player 1 wins'
+            } else if (player.health < enemy.health) {
+                document.querySelector('#displayText').innerHTML = 'Player 2 wins'
+            }
+}
+
+
+let timer = 61
+let timerId
+
+function decreaseTimer() {
+    timerId = setTimeout(decreaseTimer, 1000)
+    if (timer > 0 ) { 
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+
+
+    if (timer === 0) { 
+        determineWinner({ player, enemy, timerId })
+        } 
+    }
